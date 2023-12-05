042aed654b7bc303fead76ed7cdc6d6a1b6489d7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 19:29:47 +0500

    mapper for create admin

---
+++ src/LibraryProject/Library.Application/Mapper/MapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Library.Application.UseCases.Users.Commands.CreateAdmin;
+
+namespace Library.Application.Mapper;
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<User, CreateAdminCommand>().ReverseMap();
+    }
+}
