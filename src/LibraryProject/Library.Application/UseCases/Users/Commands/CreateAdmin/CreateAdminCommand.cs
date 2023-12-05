e97f95836d6d7c0ad7e336a09991289ec23fad39
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 19:30:24 +0500

    create admin comand created

---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/CreateAdmin/CreateAdminCommand.cs
@@ -0,0 +1,14 @@
+﻿
+using Library.Application.Abstractions.Messaging.Commands;
+
+namespace Library.Application.UseCases.Users.Commands.CreateAdmin;
+public class CreateAdminCommand : ICommand<bool>
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string UserName { get; private set; }
+    public string Password { get; private set; }
+    public string Phone { get; private set; }
+
+}
