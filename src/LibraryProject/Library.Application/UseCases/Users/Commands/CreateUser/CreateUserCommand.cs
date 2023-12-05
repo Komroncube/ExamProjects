d4086adc8b2bfdea567a6c031aca4083cd5d8d26
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 20:39:29 +0500

    create user command and handler for admins

---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using Library.Domain.Enums;
+
+namespace Library.Application.UseCases.Users.Commands.CreateUser;
+public class CreateUserCommand : ICommand<bool>
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string UserName { get; private set; }
+    public string Password { get; private set; }
+    public string Phone { get; private set; }
+    public Role Role { get; private set; }
+}
---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+namespace Library.Application.UseCases.Users.Commands.CreateUser;
+public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly IMapper _mapper;
+
+    public CreateUserCommandHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+    {
+        _applicationDbContext = applicationDbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var checkUser = _applicationDbContext.Users.FirstOrDefaultAsync(x => x.UserName == request.UserName, cancellationToken);
+        if (checkUser is null)
+        {
+            throw new InvalidOperationException("Username already exits");
+        }
+        User user = _mapper.Map<User>(request);
+
+        await _applicationDbContext.Users.AddAsync(user, cancellationToken);
+        int result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        return result > 0;
+    }
+}
