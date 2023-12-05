12909b1de8a2bbdf0bf5fd84303e445671f28256
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 19:31:06 +0500

    user model: added ctor

--- src/LibraryProject/Library.Domain/Entities/User.cs
+++ src/LibraryProject/Library.Domain/Entities/User.cs
@@ -9,7 +9,25 @@ public class User : BaseEntity
     public string UserName { get; private set; }
     public string Password { get; private set; }
     public string Phone { get; private set; }
-    public Role Role { get; private set; }
+    public Role Role { get; private set; } = Role.Student;
 
     public void SetPassword(string password) => Password = password;
+
+    public User(string firstname, string lastname, string email, string username, string password, string phone)
+    {
+        FirstName = firstname;
+        LastName = lastname;
+        Email = email;
+        UserName = username;
+        Password = password;
+        Phone = phone;
+    }
+    public void SetRole(Role role)
+    {
+        if(this.Role == role)
+        {
+            throw new InvalidOperationException("Role alread been set");
+        }
+        Role = role;
+    }
 }
