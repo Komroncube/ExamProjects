eecea96942589cfad44f7fcc212611f04576c6da
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 20:40:08 +0500

    methods for get and post created

--- src/LibraryProject/Library.API/Controllers/UsersController.cs
+++ src/LibraryProject/Library.API/Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+using Library.Application.UseCases.Users.Commands.CreateStudent;
+using Library.Application.UseCases.Users.Commands.CreateUser;
 using Library.Application.UseCases.Users.Queries.GetAllUsers;
 using Library.Domain.Entities;
 using MediatR;
@@ -16,9 +18,28 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public async ValueTask<IActionResult> GetAllUser()
+    public async ValueTask<IActionResult> GetAllUsers(CancellationToken cancellationToken)
     {
-        IEnumerable<User> users = await _mediator.Send(new GetAllUsersQuery());
+        IEnumerable<User> users = await _mediator.Send(new GetAllUsersQuery(), cancellationToken);
         return Ok(users);
+    }
+
+    /// <summary>
+    /// faqat admin tomonidan yaratiladi
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async ValueTask<IActionResult> AddAdmin(CreateUserCommand command)
+    {
+        bool result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateStudent(CreateStudentCommand command)
+    {
+        bool result = await _mediator.Send(command);
+        return Ok(result);
     }
 }
