2d5c88aeb6ec0645ae44090e91bedab7c096dd50
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 19:29:35 +0500

    dbcontext

--- src/LibraryProject/Library.Application/Abstrations/IApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Library.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace Library.Application.Abstrations;
-public interface IApplicationDbContext
-{
-    public DbSet<User> Users { get; set; }
-    public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
-}
