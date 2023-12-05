52533eb742d71adbe5a56622335d140e3f05cb07
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-05 19:30:46 +0500

    namespace modified

--- src/LibraryProject/Library.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQuery.cs
+++ src/LibraryProject/Library.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQuery.cs
@@ -1,4 +1,6 @@
-﻿namespace Library.Application.UseCases.Users.Queries.GetUserById;
+﻿using Library.Application.Abstractions.Messaging.Queries;
+
+namespace Library.Application.UseCases.Users.Queries.GetUserById;
 public class GetUserByIdQuery : IQuery<User>
 {
 
--- src/LibraryProject/Library.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQueryHandler.cs
+++ src/LibraryProject/Library.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Application.Abstractions.Messaging.Queries;
 
 namespace Library.Application.UseCases.Users.Queries.GetUserById;
 public class GetUserByIdQueryHandler : IQueryHandler<GetUserByIdQuery, User>
