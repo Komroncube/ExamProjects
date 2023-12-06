0c83ea86837fc74e8790f1822cf90136a02e8282
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:59:55 +0500

    delete user command added

---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Library.Application.UseCases.Users.Commands.DeleteUser;
+public class DeleteUserCommand : ICommand<bool>
+{
+    public int Id { get; set; }
+}
---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Library.Application.UseCases.Users.Commands.DeleteUser;
+public class DeleteUserCommandHandler : ICommandHandler<DeleteUserCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public DeleteUserCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        User? user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+        if(user is null)
+        {
+            throw new ArgumentException("User not found");
+        }
+        _applicationDbContext.Users.Remove(user);
+        int result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        return result > 0;
+    }
+}
