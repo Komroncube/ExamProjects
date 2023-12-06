81be39ca0489d965edfadad4f7cebdc50293700b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:27:24 +0500

    update method added

--- src/LibraryProject/Library.Domain/BaseEntity.cs
+++ src/LibraryProject/Library.Domain/BaseEntity.cs
@@ -4,4 +4,8 @@ public abstract class BaseEntity
     public int Id { get; private set; }
     public DateTime CreatedDate { get; private set; } = DateTime.Now;
     public DateTime? UpdatedDate { get; private set; }
+    public virtual void UpdateEntity()
+    {
+        UpdatedDate = DateTime.Now;
+    }
 }
