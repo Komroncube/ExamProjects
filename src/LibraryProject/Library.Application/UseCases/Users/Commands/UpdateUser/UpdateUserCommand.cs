232160388cecf837b286d31dd18e9b07db1044a9
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:26:42 +0500

    update command added

---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Application.UseCases.Users.Commands.UpdateUser;
+public class UpdateUserCommand : ICommand<User>
+{
+    //username bo'yicha update bo'ladi
+    public string UserName { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+}
---
+++ src/LibraryProject/Library.Application/UseCases/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.UseCases.Users.Commands.UpdateUser;
+public class UpdateUserCommandHandler : ICommandHandler<UpdateUserCommand, User>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly IMapper _mapper;
+
+    public UpdateUserCommandHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+    {
+        _applicationDbContext = applicationDbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<User> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        User? user = await _applicationDbContext.Users.FirstOrDefaultAsync(x=>x.UserName == request.UserName, cancellationToken);
+        if(user is null)
+        {
+            throw new InvalidOperationException("User not found");
+        }
+        _mapper.Map(request, user);
+        user.UpdateEntity();
+        int result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        return result > 0 ? user : throw new Exception();    
+    }
+}
