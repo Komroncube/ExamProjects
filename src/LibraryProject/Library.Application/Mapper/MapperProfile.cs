033dc7a9757b8cca3e6e084ec5496ec9307df08a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:27:13 +0500

    mapper for update

--- src/LibraryProject/Library.Application/Mapper/MapperProfile.cs
+++ src/LibraryProject/Library.Application/Mapper/MapperProfile.cs
@@ -2,15 +2,19 @@
 using Library.Application.UseCases.Students.Commands.CreateStudent;
 using Library.Application.UseCases.Students.Commands.UpdateStudent;
 using Library.Application.UseCases.Users.Commands.CreateUser;
+using Library.Application.UseCases.Users.Commands.UpdateUser;
 
 namespace Library.Application.Mapper;
 public class MapperProfile : Profile
 {
     public MapperProfile()
     {
-        CreateMap<User, CreateStudentCommand>().ReverseMap();
         CreateMap<User, CreateUserCommand>().ReverseMap();
+        CreateMap<User, UpdateUserCommand>().ReverseMap();
 
+
+
+        CreateMap<User, CreateStudentCommand>().ReverseMap();
         CreateMap<User, UpdateStudentCommand>().ReverseMap();
     }
 }
