0cfefb1359362d723f41e4aee4a563536b2bd050
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:59:38 +0500

    update user by Id

--- src/LibraryProject/Library.Application/UseCases/Students/Commands/UpdateStudent/UpdateStudentCommand.cs
+++ src/LibraryProject/Library.Application/UseCases/Students/Commands/UpdateStudent/UpdateStudentCommand.cs
@@ -1,13 +1,12 @@
 ﻿namespace Library.Application.UseCases.Students.Commands.UpdateStudent;
 public class UpdateStudentCommand : ICommand<bool>
 {
-
-    //username bo'yicha update bo'ladi
-    public string UserName { get; set; }
+    public int Id { get; set; }
 
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string Phone { get; set; }
+    public string UserName { get; set; }
 }
