be0f71aafbc4262d5f2f7f4a3f1ee83f12683892
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-06 15:59:25 +0500

    user model completed

--- src/LibraryProject/Library.API/Controllers/UsersController.cs
+++ src/LibraryProject/Library.API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using Library.Application.UseCases.Users.Commands.CreateUser;
+using Library.Application.UseCases.Users.Commands.DeleteUser;
 using Library.Application.UseCases.Users.Commands.UpdateUser;
 using Library.Application.UseCases.Users.Queries.GetAllUsers;
+using Library.Application.UseCases.Users.Queries.GetUserById;
 using Library.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +25,12 @@ public class UsersController : ControllerBase
         IEnumerable<User> users = await _mediator.Send(new GetAllUsersQuery());
         return Ok(users);
     }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetUserById(GetUserByIdQuery getUserByIdQuery)
+    {
+        User user = await _mediator.Send(getUserByIdQuery);
+        return Ok(user);
+    }
 
     /// <summary>
     /// faqat admin tomonidan yaratiladi
@@ -41,6 +49,13 @@ public class UsersController : ControllerBase
         User user = await _mediator.Send(updateUserCommand);
         return Ok(user);
     }
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteUser(DeleteUserCommand deleteUserCommand)
+    {
+        bool isDeleted = await _mediator.Send(deleteUserCommand);
+        return Ok(isDeleted);
+    }
+    
 
 
 }
