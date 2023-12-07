18d09f12616ead77e0ca2063683271a667c6ba24
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-07 20:38:56 +0500

    keys for caching

---
+++ src/LibraryProject/Library.Domain/Enums/CacheKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Enums;
+public static class CacheKeys
+{
+    public const string BOOKSKEY = "BOOKS";
+    public const string USERSKEY = "USERS";
+    public const string WAITLIST = "WAITINGS";
+}
