3e603af5b29897fe90184749111463ee8b96be4a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-07 09:23:30 +0500

    extension method for jwt layer

---
+++ src/JWTLayer/JwtTokenProvider/AddJwtLayer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtTokenProvider;
+public static class AddJwtLayer
+{
+    public static IServiceCollection AddCustomJwtLayer(this IServiceCollection services)
+    {
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwt =>
+        {
+            jwt.RequireHttpsMetadata = false;
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtTokenHandler.JWT_SECURITY_KEY)),
+                ClockSkew = TimeSpan.Zero
+            };
+
+        });
+        return services;
+    }
+}
