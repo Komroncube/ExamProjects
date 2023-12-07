3808c5935e6cf4bc41798ee35c23ff44280e0280
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-07 20:32:18 +0500

    handler working with cache

--- src/LibraryProject/Library.Application/UseCases/Users/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
+++ src/LibraryProject/Library.Application/UseCases/Users/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -1,17 +1,25 @@
-﻿namespace Library.Application.UseCases.Users.Queries.GetAllUsers;
+﻿using Library.Application.Services;
+
+namespace Library.Application.UseCases.Users.Queries.GetAllUsers;
 public class GetAllUsersQueryHandler : IQueryHandler<GetAllUsersQuery, IEnumerable<User>>
 {
     private readonly IApplicationDbContext _applicationDbContext;
+    private readonly ICacheService _cacheService;
 
-    public GetAllUsersQueryHandler(IApplicationDbContext applicationDbContext)
+    public GetAllUsersQueryHandler(IApplicationDbContext applicationDbContext, ICacheService cacheService)
     {
         _applicationDbContext = applicationDbContext;
+        _cacheService = cacheService;
     }
 
     public async Task<IEnumerable<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
     {
-        IEnumerable<User> users = await _applicationDbContext.Users.ToListAsync();
-
+        var users = await _cacheService.GetDataAsync<IEnumerable<User>>(CacheKeys.USERSKEY); //await _applicationDbContext.Users.ToListAsync();
+        if(users is null)
+        {
+            users = await _applicationDbContext.Users.ToListAsync(cancellationToken);
+            await _cacheService.SetDataAsync(CacheKeys.USERSKEY, users, TimeSpan.FromMinutes(10));
+        }
         return users;
     }
 }
