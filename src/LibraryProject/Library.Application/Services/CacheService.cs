359a0bf89a661fef0643fbd5e2a40c84a5b363b5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-07 20:30:23 +0500

    working with caches

---
+++ src/LibraryProject/Library.Application/Services/CacheService.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Library.Application.Services;
+public class CacheService : ICacheService
+{
+    private readonly IDatabase _cacheDatabase;
+
+    public CacheService()
+    {
+        string REDIS_HOST_NAME = Environment.GetEnvironmentVariable("REDIS_HOST_NAME") ?? "redis";
+        var redis = ConnectionMultiplexer.Connect($"{REDIS_HOST_NAME}:6379");
+        _cacheDatabase = redis.GetDatabase();
+    }
+
+
+
+    public async ValueTask<T?> GetDataAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        var value = await _cacheDatabase.StringGetAsync(key);
+        if (!string.IsNullOrEmpty(value))
+        {
+            return JsonConvert.DeserializeObject<T>(value,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new PrivateResolver()
+                });
+        }
+        return default;
+
+    }
+
+    public async ValueTask<bool> RemoveDataAsync(string key, CancellationToken cancellationToken = default)
+    {
+        bool isExist = await _cacheDatabase.KeyExistsAsync(key);
+        if (isExist)
+        {
+            return await _cacheDatabase.KeyDeleteAsync(key);
+        }
+        return false;
+    }
+
+    public async ValueTask<bool> RefreshDataAsync<T>(string key, T value, TimeSpan expiretime, CancellationToken cancellationToken = default)
+    {
+        await RemoveDataAsync(key);
+        bool isRefreshed = await SetDataAsync<T>(key, value, expiretime);
+        return isRefreshed;
+    }
+
+    public async ValueTask<bool> SetDataAsync<T>(string key, T value, TimeSpan expiretime, CancellationToken cancellationToken = default)
+    {
+        bool isSet = await _cacheDatabase.StringSetAsync(key, JsonConvert.SerializeObject(value), expiretime);
+        return isSet;
+    }
+}
---
+++ src/LibraryProject/Library.Application/Services/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Application.Services;
+public interface ICacheService
+{
+    public ValueTask<T?> GetDataAsync<T>(string key, CancellationToken cancellationToken = default);
+    public ValueTask<bool> RemoveDataAsync(string key, CancellationToken cancellationToken = default);
+    public ValueTask<bool> RefreshDataAsync<T>(string key, T value, TimeSpan expiretime, CancellationToken cancellationToken = default);
+    public ValueTask<bool> SetDataAsync<T>(string key, T value, TimeSpan expiretime, CancellationToken cancellationToken = default);
+}
