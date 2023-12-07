9e05ac4052a26c95297b356632ba938843d3df3d
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-07 21:39:58 +0500

    usings removed

--- src/JWTLayer/JwtTokenProvider/Models/AuthenticationResponse.cs
+++ src/JWTLayer/JwtTokenProvider/Models/AuthenticationResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JwtTokenProvider.Models;
+﻿namespace JwtTokenProvider.Models;
 public class AuthenticationResponse
 {
     public string Username { get; set; }
