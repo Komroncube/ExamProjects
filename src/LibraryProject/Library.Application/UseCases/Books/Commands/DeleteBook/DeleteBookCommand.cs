c259c99846f6992755b797d67b7129976cb46f73
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-08 10:44:59 +0500

    delete book command

---
+++ src/LibraryProject/Library.Application/UseCases/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.UseCases.Books.Commands.DeleteBook;
+public class DeleteBookCommand : ICommand<bool>
+{
+    public int Id { get; set; }
+}
---
+++ src/LibraryProject/Library.Application/UseCases/Books/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Application.UseCases.Books.Commands.DeleteBook;
+public class DeleteBookCommandHandler : ICommandHandler<DeleteBookCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public DeleteBookCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        var book = await _applicationDbContext.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
+        if (book == null)
+        {
+            throw new Exception("Book not found");
+        }
+        _applicationDbContext.Books.Remove(book);
+        int result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        return result > 0;
+
+    }
+}
