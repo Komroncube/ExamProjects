e65aa64fc8037a02f74f808474fcbcbdd8710563
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-08 10:45:49 +0500

    mapper for books

--- src/LibraryProject/Library.Application/Mapper/MapperProfile.cs
+++ src/LibraryProject/Library.Application/Mapper/MapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Application.UseCases.Books.Commands.CreateBook;
+using Library.Application.UseCases.Books.Commands.UpdateBook;
 using Library.Application.UseCases.Students.Commands.CreateStudent;
 using Library.Application.UseCases.Students.Commands.UpdateStudent;
 using Library.Application.UseCases.Users.Commands.CreateUser;
@@ -9,15 +10,18 @@ public class MapperProfile : Profile
 {
     public MapperProfile()
     {
+        //user
         CreateMap<User, CreateUserCommand>().ReverseMap();
         CreateMap<User, UpdateUserCommand>().ReverseMap();
 
 
-
+        //student
         CreateMap<User, CreateStudentCommand>().ReverseMap();
         CreateMap<User, UpdateStudentCommand>().ReverseMap();
-
+        
+        //book
         CreateMap<Book, CreateBookCommand>().ReverseMap();
+        CreateMap<Book, UpdateBookCommand>().ReverseMap();
 
     }
 }
