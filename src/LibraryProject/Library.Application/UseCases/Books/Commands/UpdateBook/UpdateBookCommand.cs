67f6c2ada99066cf63343f3dec0f40ba31fba59e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-08 10:41:08 +0500

    updatebook command created

---
+++ src/LibraryProject/Library.Application/UseCases/Books/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.UseCases.Books.Commands.UpdateBook;
+public class UpdateBookCommand : ICommand<Book>
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public string Category { get; set; }
+
+}
---
+++ src/LibraryProject/Library.Application/UseCases/Books/Commands/UpdateBook/UpdateBookCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.UseCases.Books.Commands.UpdateBook;
+public class UpdateBookCommandHandler : ICommandHandler<UpdateBookCommand, Book>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly IMapper _mapper;
+
+    public UpdateBookCommandHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+    {
+        _applicationDbContext = applicationDbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<Book> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+    {
+        Book? book = await _applicationDbContext.Books.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        if (book == null)
+        {
+            throw new Exception("Book not found");
+        }
+        _mapper.Map(request, book);
+        await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        return book;
+    }
+}
